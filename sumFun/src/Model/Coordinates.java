eb073b30ab2a10bed14c9cce3ea90e02565e1b03
Aneesh Raman <aneeshraman97@gmail.com>
2017-10-23 15:54:55 -0400

    Added coordinates class

---
+++ sumFun/src/Model/Coordinates.java
@@ -0,0 +1,29 @@
+package Model;
+
+public class Coordinates {
+	private int rows;
+	private int cols;
+	
+	public Coordinates(int rows,int cols){
+		this.rows=rows;
+		this.cols=cols;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getCols() {
+		return cols;
+	}
+
+	public void setCols(int cols) {
+		this.cols = cols;
+	}
+	
+	
+}
