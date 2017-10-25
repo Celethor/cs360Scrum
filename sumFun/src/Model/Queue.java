4de56b9925a567328bb96ddbb61a9e97a6fd82fd
unknown <ruizmf01@students.ipfw.edu>
2017-10-24 20:55:00 -0400

    reworked Queue class to use ArrayList

--- sumFun/src/Model/Queue.java
+++ sumFun/src/Model/Queue.java
@@ -1,68 +1,44 @@
 package Model;
 
-public class Queue {
-	/* class implements a queue using a circular array
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Queue<E> extends ArrayList<E>{
+	/* class implements a queue using a linked list
 	 * 
 	 */
 	
-	//queue data member will be the circular array
-	private int[] queue;
+	//Data Members
+	private ArrayList<E> queue;
 	
-	//the front of the queue, this item will be returned upon dequeue
-	private int read = 0;
-	
-	//the end of the queue, this is where numbers will be stored upon queue operation
-	private int write = 0;
-	
-	//constructor will create a queue of specified size
-	//size+1 is included as in implementation one array element is always empty
-	//
-	public Queue(int size){
-		this.queue = new int[size+1];
+	//constructor
+	public Queue() {
+		queue = new ArrayList<E>();
 	}
 	
-	//enqueue adds integer to the write of the queue
-	public void enqueue(int x) throws Exception{
-		if (read == (write + 1) % (queue.length)){
-			throw new Exception("Queue is full");
-		}
-		queue[write] = x;
-		write = (write + 1) % (queue.length);
+	//Enqueue adds integer to the end of queue
+	private void enqueue(E element) {
+		queue.add(element);
 	}
 	
-	//returns the element at the fromt of the queue
-	public int dequeue() throws Exception{
-		int result;
+	//Dequeue removes element form the front of the queue
+	private E dequeue() {
+		return queue.remove(0);
+	}
+	
+	//getElement returns element 
+	private E getElement(int index) {
+		return queue.get(index);
+	}
+	
+	//used to print elements of ArrayList<E>
+	private String printQueue() {
+		String answer = "";
+		answer = answer + Arrays.toString(queue.toArray());
 		
-		if(read == write){
-			throw new Exception("Queue is empty");
-		}
-		result = queue[read];
-		queue[read] = -1;
+		return answer;
 		
-		read = (read+1)%(queue.length);
-		
-		return result;
 	}
 	
-	//returns item in queue at element
-	public int getQueueElement(int index){
-		int element;
-		element = queue[(read + index) % queue.length];
-		return element;
-	}
-	
-	//toString method in order to test Circular Array
-	//does not include empty element as it is never filled and is not 
-	//relevent outside of making sure array stays within specified size
-
-	public String toString(){
-		String result = "HEAD--> [ ";
-		int i;
-		for(i=0; i<queue.length-1; i++){
-			result = result + getQueueElement(i) + ", ";
-		}
-		result = result + "] <-- TAIL";
-		return result;
-	}
 }
