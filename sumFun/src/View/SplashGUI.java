d259396ce8b68e3b62d27f3f3121b04e9626a5a3
Aneesh Raman <aneeshraman97@gmail.com>
2017-11-16 11:00:16 -0500

    Changed SplashGUI to launch NewGUI

--- sumFun/src/View/SplashGUI.java
+++ sumFun/src/View/SplashGUI.java
@@ -44,7 +44,7 @@ public class SplashGUI extends JFrame {
 		btnPlayTimed.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				new GUI(Game.getGame("timed"));
+				new NewGUI(Game.getGame("timed"));
 				setVisible(false);	
 			}
 		});
@@ -53,7 +53,7 @@ public class SplashGUI extends JFrame {
 		btnPlayUntimed.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				new GUI(Game.getGame("untimed"));
+				new NewGUI(Game.getGame("untimed"));
 				setVisible(false);	
 			}
 		});
