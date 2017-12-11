ca9e0344c33b8ace5c80dec96ea12b0b1335bf78
unknown <ruizmf01@students.ipfw.edu>
2017-12-11 12:15:03 -0500

    added header doc for whitebox test updateTime

--- sumFun/src/test/whitebox/TestUpdateTime.java
+++ sumFun/src/test/whitebox/TestUpdateTime.java
@@ -10,6 +10,22 @@ import org.junit.Test;
 
 public class TestUpdateTime {
 	
+	/**
+	 * Whitebox testing, Independent Path testing, for updateTime
+	 * 
+	 * Update time checks current amount of time left in the game and updates the time accordingly
+	 * If the time limit is up or at 0 then the game is over.
+	 * If time limit is not up then the limeLimit is reduced by one.
+	 * To test all paths it was necessary for us to use two cases with input TimeLimit > 0 and TimeLimit = 0.
+	 * 
+	 * Test Cases:
+	 * 
+	 * TimeLimit(Before)	TimeLimit(After)	TimeRemaining(Before)	TimeRemaining(After)	Case
+	 *      60					59						"1 : 00"				"0 : 59"		  A
+	 *       0					0						"0 : 00"				"0 : 00" 		  B
+	 * 
+	 */
+	
 	static Game test;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -30,6 +46,7 @@ public class TestUpdateTime {
 		test.getTimer().updateTime();
 		assertEquals(test.isGameOver(), false);
 		assertEquals(test.getTimer().getTimeLimit(), 59);
+		assertEquals("0 : 59",test.getRemainingTime());
 	}
 	
 	/**
@@ -46,6 +63,7 @@ public class TestUpdateTime {
 		test.getTimer().updateTime();
 		assertEquals(test.isGameOver(), true);
 		assertEquals(test.getTimer().getTimeLimit(), 0);
+		assertEquals("0 : 00",test.getRemainingTime());
 	}
 
 }
